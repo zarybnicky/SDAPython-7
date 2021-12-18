39aa714a0c77df499fb6241fa4db4ac8ed36b18a
ZuzanaStecikova <zuzanastecikova@post.sk>
2021-12-18 13:15:48 +0100

    test  prime factorial

--- 1-algo-prime-factors/stecikova2.py
+++ 1-algo-prime-factors/stecikova2.py
@@ -1,33 +1,18 @@
 from typing import List
-import math
 
 primes = [2, 3, 5, 7, 11, 13, 17]
 
-# Below function will print the
-# all prime factor of given number
-def get_prime_factors(num):
-    # Using the while loop, we will print the number of two's that divide n
-    while num % 2 == 0:
-        print(2, )
-        num = num / 2
 
-    for i in range(3, int(math.sqrt(num)) + 1, 2):
-
-        # while i divides n , print i ad divide n
-        while num % i == 0:
-            print(i, )
-            num = num / i
-    if num > 2:
-        print(num)
-    # calling function
-
-
-num = 200
-get_prime_factors(num)
-
-# def get_prime_factors(n: int) -> List[int]:
-#     return []
-
+def get_prime_factors(n: int) -> List[int]:
+    factors = []
+    for prime in primes:
+        if n % prime != 0:
+            continue
+        else:
+            while n % prime == 0:
+                n = n // prime
+                factors.append(prime)
+    return factors
 
 # <editor-fold defaultstate="collapsed" desc="Tests">
 def test_prime_factors():
