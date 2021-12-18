fff16dc6a981ea5c541fa854ae09ff8ad08c7c99
milan <vrbjar@icloud.com>
2021-12-18 12:11:30 +0100

    prime

---
+++ 1-algo-prime-factors/iam61 prime.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+# 4 = 2 * 2
+# 9 = 3 * 3
+# 27 = 3 * 3 * 3
+
+primes = [2, 3, 5, 7, 11, 13, 17]
+
+
+def get_prime_factors(n: int) -> List[int]:
+    factors = []
+    for prime in primes:
+        while n % prime == 0:
+            factors.append(prime)
+            n /= prime
+    return factors
+
+# <editor-fold defaultstate="collapsed" desc="Tests">
+def test_prime_factors():
+    assert sorted(get_prime_factors(1)) == []
+    assert sorted(get_prime_factors(4)) == [2, 2]
+    assert sorted(get_prime_factors(17)) == [17]
+    assert sorted(get_prime_factors(27)) == [3, 3, 3]
+# </editor-fold>
