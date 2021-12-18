3227977504a7d790a1da2b4e9628b0ab38e8c4bd
Katerina Kratka <katka.kratkaa@gmail,com>
2021-12-18 11:19:07 +0100

    faktorial

--- 1-algo-factorial/kachnazkoci.py
+++ 1-algo-factorial/kachnazkoci.py
@@ -1,13 +1,15 @@
-def factorial(num: int) -> int:
+def factorial(n: int) -> int:
     factorial = 1
-    if num < 0:
+    if n < 0:
         print("Sorry, záporný čísla ne-e")
-    elif num == 0:
+    elif n == 0:
+        return 1
         print("Faktoriál 0 je 1")
     else:
-        for i in range(1, num + 1):
+        for i in range(1, n + 1):
             factorial = factorial * i
-        print("Faktorial č.", num, "je", factorial)
+        print("Faktorial č.", n, "je", factorial)
+        return factorial
 
 factorial(7)
 # <editor-fold defaultstate="collapsed" desc="Tests">
